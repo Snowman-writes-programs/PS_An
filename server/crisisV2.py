8a1da8aefff4412ff2b0dc83f8107100e277d768
Twelve <bionicvnb@gmail.com>
2023-11-08 07:08:29 +0700

    dummy code

--- server/crisisV2.py
+++ server/crisisV2.py
@@ -1,2 +1,15 @@
 from flask import request
 
+def CrisisV2Function():
+
+    data = request.data
+
+    data = {
+        "crisisV2List": [],
+        "playerDataDelta": {
+            "modified": {},
+            "deleted": {}
+        }
+    }
+
+    return data
