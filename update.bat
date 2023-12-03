57b8d3135f39be5d1520f2b856167ec4a359f34f
Twelve <bionicvnb@gmail.com>
2023-12-03 21:43:48 +0700

    test

--- update.bat
+++ update.bat
@@ -7,3 +7,12 @@ call env\scripts\activate.bat
 
 python3 update_config.py
 python3 update_game.py
+
+git clone -n --depth=1 --filter=tree:0 https://github.com/Kengxxiao/ArknightsGameData.git
+cd ArknightsGameData/
+git sparse-checkout set --no-cone zh_CN/gamedata/excel/
+git checkout
+
+mv zh_CN/gamedata/excel data/
+
+rm -r ArknightsGameData/
